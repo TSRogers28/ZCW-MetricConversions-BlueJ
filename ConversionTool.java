45087177f9ed5730809ab303775d07d391964a30
Tommy Rogers <tommyr@TommyRogers.local>
2018-05-30 11:15:06 -0400

    made it better

--- ConversionTool.java
+++ ConversionTool.java
@@ -1,8 +1,12 @@
 public class ConversionTool {
-    enum metrics{}
-
+    static final float CM2IN = 0.3937007874f;
+    static final float IN2CM = 2.54f;
+    static final float FT2MT = 0.3048f;
+    static final float MT2FT = 3.280839895f;
+    static final float MPH2KPH = 1.609344f;
+    static final float KPH2MPH = 0.6213712f;
     
-
+    
     public static void main(String[] args){}
 
     public static float CelsiusToFahrenheit(float celsius){
@@ -13,57 +17,40 @@ public class ConversionTool {
         return (fahrenheit - 32) * 5 / 9;
     }
 
-    public static float CentimetersToInches(float centimeters){
-        float CM2IN =  0.3937007874f;
-        if(centimeters <= 0f){
-            return 0;
+    public static float checkForZeroOrNegative(float input){
+        if(input <= 0){
+            input = 0;
         }
-        else
+        return input;
+    }
+    
+    public static float CentimetersToInches(float centimeters){
+        centimeters = checkForZeroOrNegative(centimeters);
         return centimeters * CM2IN;
     }
 
     public static float InchesToCentimeters(float inches){
-        float IN2CM =  2.54f;
-        if(inches <= 0){
-            return 0;
-        }
-        else
+        inches = checkForZeroOrNegative(inches);
         return inches * IN2CM;
     }
 
     public static float FeetToMeters(float feet){
-        float FT2MT =   0.3048f;
-        if(feet <= 0){
-            return 0;
-        }
-        else
+        feet = checkForZeroOrNegative(feet);
         return feet * FT2MT;
     }
 
     public static float MetersToFeet(float meters){
-        float MT2FT = (float) 3.280839895f;
-        if(meters <= 0){
-            return 0;
-        }
-        else
+        meters = checkForZeroOrNegative(meters);
         return meters * MT2FT;
     }
 
     public static float MphToKph(float mph){
-        float MPH2KPH = 1.609344f;
-        if(mph <= 0){
-            return 0;
-        }
-        else
+        mph = checkForZeroOrNegative(mph);
         return mph * MPH2KPH;
     }
 
     public static float KphToMph(float kph){
-        float KPH2MPH = 0.6213711922f;
-        if(kph <= 0){
-            return 0;
-        }
-        else
+        kph = checkForZeroOrNegative(kph);
         return kph * KPH2MPH;
     }
 }
